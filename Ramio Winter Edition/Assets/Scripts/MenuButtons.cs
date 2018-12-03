024556a344a986f681f32e2dd183d7b475286231
Brexton Lane <19.brexton.lane@ksd.org>
2018-12-03 09:55:18 -0800

    replaced menubuttons and added tags

--- Ramio Winter Edition/Assets/Scripts/MenuButtons.cs
+++ Ramio Winter Edition/Assets/Scripts/MenuButtons.cs
@@ -3,23 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuButtons : MonoBehaviour {
-    public int lives = 3;
-    void Start ()
+public class MenuButtons : MonoBehaviour
+{
+    void Update()
     {
-        PlayerPrefs.SetInt("Lives", lives);
+        // check to see if p key is pressed
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+
+            //if is pressed stop stuff from moving
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                //make pause menu visible
+                GetComponent<Canvas>().enabled = true;
+            }
+            else if (Time.timeScale == 0)
+            {
+                //unpause
+                Resume();
+            }
+        }
     }
+
     public void NewGame()
     {
         SceneManager.LoadScene("Level1");
     }
+
     public void QuitGame()
     {
         Application.Quit();
     }
 
-    public void ReturnToMain()
+    public void Resume()
     {
-        SceneManager.LoadScene("MainMenu");
+        GetComponent<Canvas>().enabled = false;
+        Time.timeScale = 1;
     }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+
+    }
+
 }
