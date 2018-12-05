62e108a82cfd06d0f9e5fa6723219788f86c72cc
Alex Wise <19.alex.wise@ksd.org>
2018-12-05 09:47:36 -0800

    Made snowball script

---
+++ Ramio Winter Edition/Assets/Scripts/SnowballSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowballSpawner : MonoBehaviour {
+
+    public GameObject Snowball;
+    public Vector2 ThrowDirection = new Vector2();
+    float ThrowDelay = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ThrowDelay -= Time.deltaTime;
+        if(ThrowDelay <= 0)
+        {
+            Instantiate(Snowball, transform.position, Quaternion.identity);
+        }
+	}
+}
