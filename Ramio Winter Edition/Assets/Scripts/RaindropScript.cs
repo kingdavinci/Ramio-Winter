836b04d13d552a0bfeae742e5e73cc19f83f5f54
Alex Wise <19.alex.wise@ksd.org>
2018-12-13 09:02:07 -0800

    Even more raindrop improvements

---
+++ Ramio Winter Edition/Assets/Scripts/RaindropScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaindropScript : MonoBehaviour {
+
+    public GameObject Splash;
+    public float Lifetime = 4f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Lifetime -= Time.deltaTime;
+        if (Lifetime <= 0)
+        {
+            Instantiate(Splash, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Instantiate(Splash, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
