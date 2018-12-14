56ba814fe8f5eb203d3fdfac5f011dafb418b55b
Kent Melville <19.kent.melville@ksd.org>
2018-12-14 07:41:35 -0800

    update

---
+++ Ramio Winter Edition/Assets/Scripts/LoadLevel4.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadLevel4 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //attaching this script to the portal
+        //will load the win screen when player collides
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Level4");
+        }
+    }
+}
